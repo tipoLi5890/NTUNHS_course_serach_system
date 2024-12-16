ca9703dfd28643376a689cf7e05bcd003d373fdc
YSC <jinghong11138@gmail.com>
2024-12-16 20:02:11 +0800

    record響應式，在record.jsx中增加import 'courses.css'

--- React/src/pages/record/Record.jsx
+++ React/src/pages/record/Record.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
+import '../courses/courses.css'
 import './record.css';
 import courseImage from "../../assets/courses/course.png";
 
