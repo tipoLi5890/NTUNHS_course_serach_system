f69314b44106e1d6608410f8464fc01471af33ad
eric31415926535 <134913750+eric31415926535@users.noreply.github.com>
2024-12-19 11:55:47 +0800

    課程規劃新增同步收藏功能

--- PHP/DB/用戶收藏.sql
+++ PHP/DB/用戶收藏.sql
@@ -4,7 +4,6 @@ CREATE TABLE 用戶收藏 (
     用戶ID INT NOT NULL,                             -- 外鍵，指向用戶表
     課程ID INT NOT NULL,                             -- 外鍵，指向課程表
     收藏時間 TIMESTAMP DEFAULT CURRENT_TIMESTAMP,    -- 收藏時間
-    放置狀態 ENUM('0', '1') DEFAULT '0',             -- isPlaced在這
     FOREIGN KEY (用戶ID) REFERENCES 用戶(用戶ID) ON DELETE CASCADE,
     FOREIGN KEY (課程ID) REFERENCES 課程(編號) ON DELETE CASCADE,
     UNIQUE (用戶ID, 課程ID)                          -- 保證每個用戶對課程只能收藏一次
