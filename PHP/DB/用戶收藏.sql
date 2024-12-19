95722a0ce46740b6e3509a2af1bc12d436a3f21a
eric31415926535 <134913750+eric31415926535@users.noreply.github.com>
2024-12-19 11:48:33 +0800

    用戶收藏 新增放置狀態

--- PHP/DB/用戶收藏.sql
+++ PHP/DB/用戶收藏.sql
@@ -4,6 +4,7 @@ CREATE TABLE 用戶收藏 (
     用戶ID INT NOT NULL,                             -- 外鍵，指向用戶表
     課程ID INT NOT NULL,                             -- 外鍵，指向課程表
     收藏時間 TIMESTAMP DEFAULT CURRENT_TIMESTAMP,    -- 收藏時間
+    放置狀態 ENUM('0', '1') DEFAULT '0',             -- isPlaced在這
     FOREIGN KEY (用戶ID) REFERENCES 用戶(用戶ID) ON DELETE CASCADE,
     FOREIGN KEY (課程ID) REFERENCES 課程(編號) ON DELETE CASCADE,
     UNIQUE (用戶ID, 課程ID)                          -- 保證每個用戶對課程只能收藏一次
@@ -11,8 +12,8 @@ CREATE TABLE 用戶收藏 (
 
 INSERT INTO 用戶收藏 (用戶ID, 課程ID)
 VALUES
-    (2, 101),  
-    (2, 102),  
+    (2, 11),  
+    (2, 12),  
     (2, 103), 
     (2, 104),  
-    (2, 105);  
+    (2, 105);
